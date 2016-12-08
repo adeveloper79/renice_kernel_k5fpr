0bba5ff0a1e8c88630606d343b4194bd966e88f4
Sandeep Sethi <adeveloper79@gmail.com>
2016-12-08 09:30:18 +0530

    Reinitialized Files

---
+++ start.sh
@@ -0,0 +1,4 @@
+PATH=${PATH}:~/toolchains/UBERTC/aarch64-linux-android-5.3/bin
+export ARCH=arm64
+make k5fpr_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
+make -j5 ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
