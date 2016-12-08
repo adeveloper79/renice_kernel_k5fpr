9ab7b37f90eb76d7081ab0cfd06ed5c8aa669857
Sandeep Sethi <adeveloper79@gmail.com>
2016-12-08 09:43:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#CHANGE THE TOOLCHIAN PATH TO YOUR PATHC IF YOU ARE USING THIS script to compile kernel.
 PATH=${PATH}:~/toolchains/UBERTC/aarch64-linux-android-5.3/bin
 export ARCH=arm64
 make k5fpr_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
